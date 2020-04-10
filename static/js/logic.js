e538e0cf8f0c988ab6fa4ecec150254d9b7ed5f1
aydin <aydinjalil@gmail.com>
2020-04-10 16:52:12 -0400

    made legend colors work

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,5 @@
 var coords = [37.0902, -95.7129];
-var mapZoomLevel = 3;
+var mapZoomLevel = 4;
 
 // Create the createMap function
 var myMap = L.map("map", {
@@ -20,7 +20,6 @@ var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geo
 
 
 function circle_creator (features, fill, _rad){
-	console.log(features);
 	var circle = L.shapeMarker([features.geometry.coordinates[1], features.geometry.coordinates[0]], {
 				fillColor: fill,
 				fillOpacity: 1,
@@ -30,10 +29,19 @@ function circle_creator (features, fill, _rad){
 				radius: _rad
 		}).bindPopup("<h3>" + features.properties.place +
       "</h3><hr><p>" + new Date(features.properties.time) + "</p>" + 
-      "<hr><p>" + "Magnitude: " + features.properties.mag + "</p>");
+      "<hr><p>" + "Richter Magnitude: " + features.properties.mag + "</p>");
 	return circle;
 } 
 
+
+function getValue(x) {
+    return x >= 5 ? "#ff2f45" :
+           x >= 4 ? "#ff9049" :
+           x >= 3 ? "#ffa062" :
+           x >= 2 ? "#ffe92f" :
+           x >= 1 ? "#adff2f" :
+           "#45ff2f";
+}
 
 
 d3.json(url, function(error, response){
@@ -43,43 +51,31 @@ d3.json(url, function(error, response){
 	response.features.forEach((features=>{
 		// console.log(features);
 		var mag = features.properties.mag;
-		// var markers = L.markerClusterGroup()
-		var coords = [features.geometry.coordinates[1], features.geometry.coordinates[0]]
-		// var lat = features.geometry.coordinates[1];
-		// var long = features.geometry.coordinates[0];
 
-		// console.log("Lat: " + lat + "\n" + "Long: " + long);
+		var coords = [features.geometry.coordinates[1], features.geometry.coordinates[0]]
+
 		if (coords)
-			if (mag < 2){
-				var circle = circle_creator(features, "#FFCCCC", 2*(1+mag))	
+			if (mag < 1){
+				var circle = circle_creator(features, "#45ff2f", 2*(1+mag))	
+			}
+
+			else if (mag < 2){
+				var circle = circle_creator(features, "#adff2f", 3*(1+mag))	
 			}
 
 			else if (mag < 3){
-				var circle = circle_creator(features, "#FF9999", 3*(1+mag))	
+				var circle = circle_creator(features, "#ffe92f", 4*(1+mag))	
 			}
 
 			else if (mag < 4){
-				var circle = circle_creator(features, "#FF6666", 4*(1+mag))	
+				var circle = circle_creator(features, "#ffa062", 5*(1+mag))	
 			}
 
 			else if (mag < 5){
-				var circle = circle_creator(features, "#FF3333", 5*(1+mag))	
+				var circle = circle_creator(features, "#ff9049", 6*(1+mag))	
 			}
-
-			else if (mag < 6){
-				var circle = circle_creator(features, "#FF0000", 6*(1+mag))	
-			}
-
-			else if (mag < 7){
-				var circle = circle_creator(features, "#CC0000", 7*(1+mag))	
-			}
-
-			else if (mag < 8){
-				var circle = circle_creator(features, "#990000", 8*(1+mag))	
-			}
-
-			else {
-				var circle = circle_creator(features, "#660000", 9*(1+mag))	
+			else{
+				var circle = circle_creator(features, "#ff2f45", 7*(1+mag))	
 			}
 		 circle.addTo(myMap)
 
@@ -88,30 +84,22 @@ d3.json(url, function(error, response){
 }
 );
 
-// var legend = L.control({position: 'bottomright'});
-// console.log(legend);
-// legend.onAdd = function (map) {
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function () {
+    var div = L.DomUtil.create('div', 'legend');
+    // var labels = ["0-1", "1-2", "2-3", "3-4", "4-5", "5+"];
+    var labels = []
+    var grades = [0, 1, 2, 3, 4, 5];
+    var categories = ["0-1", "1-2", "2-3", "3-4", "4-5", "5+"];
+    var colors = ["#45ff2f", "#adff2f", "#ffe92f", "#ffa062", "#ff9049", "#ff2f45"]
+    div.innerHTML += '<strong>Richter Magnitude<strong><br>';
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML += '<i style="background:' + colors[i] + '"></i>' + categories[i]+ '<br>';
+        // div.innerHTML += '<i style="background:' + getValue(grades[i]) + '"></i> ' + grades[i] + '&ndash;' + grades[i + 1] + '<br>';
+    }
 
-//     var div = L.DomUtil.create('div', 'info legend'),
-//         magnitude = ["0-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8+"],
-//         labels = [];
+    return div;
+};
 
-//     // loop through our density intervals and generate a label with a colored square for each interval
-//     for (var i = 0; i < magnitude.length; i++) {
-//         div.innerHTML +=
-//             '<i style="background:' + getColor(magnitude[i]) + '"></i>'
-//     }
+legend.addTo(myMap);
 
-//     return div;
-// };
-// function getColor(d) {
-//     return d === "0-2" ? '#FFCCCC' :
-//            d === "2-3"  ? '#FF9999' :
-//            d === "3-4"  ? '#FF6666' :
-//            d === "4-5"  ? '#FF3333' :
-//            d === "5-6"   ? '#FF0000' :
-//            d === "6-7"   ? '#CC0000' :
-//            d === "7-8"   ? '#990000' :
-//                       '##660000';
-// }
-// legend.addTo(map);
